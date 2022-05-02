d8be4723a198bda5b9147e88dca9652a9f537546
DanielDiazUnal <ddiazmo@unal.edu.co>
2022-05-02 12:48:23 -0500

    Subir archivos ramdom

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-<<<<<<< HEAD
-//  let validarRadio = document.getElementById('flexRadioDefault1')
-//validarRadio.onclick = function(){desplegar()};
-
 function desplegar(){
-    alert("Hola?")
-}
+    // let obj=document.getElementById("flexRadioDefault1");
 
-
-let verificarContrasena = document.getElementById('verificarContrasena')
-
-function validarContrasena(){
-=======
-function verificarContrasena(){
->>>>>>> 9c885524c57a9119e723e1d8b9f89bfb636d55ee
-    let contrasena = document.getElementById('contrasena')
-    let verificarContrasena = document.getElementById('verificarContrasena')
-    if (contrasena == verificarContrasena){
-        console.log("si")
-    }else{
-        console.log("no")
-    }
+    alert("asdasjdjkasd");
 }
