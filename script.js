9317cbae7267dc1c6b0c91978248b874f9dc57e6
DanielDiazUnal <ddiazmo@unal.edu.co>
2022-05-02 12:03:15 -0500

    Se hace subida de imagenes además se agrega evento

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//  let validarRadio = document.getElementById('flexRadioDefault1')
+//validarRadio.onclick = function(){desplegar()};
+
+function desplegar(){
+    alert("Hola?")
+}
+
+
 let verificarContrasena = document.getElementById('verificarContrasena')
 
 function validarContrasena(){
