40a6818fbfb7c784c37979a3514e5c97724b5cf7
Juan Giraldo <juandiegogbo@gmail.com>
2022-05-02 11:58:24 -0500

    Se agrega js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let verificarContrasena = document.getElementById('verificarContrasena')
-
-function validarContrasena(){
+function verificarContrasena(){
     let contrasena = document.getElementById('contrasena')
-    
-
+    let verificarContrasena = document.getElementById('verificarContrasena')
+    if (contrasena == verificarContrasena){
+        console.log("si")
+    }else{
+        console.log("no")
+    }
 }
-    
