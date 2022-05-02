c9840f1de4bbdb1715dbf0e10685a061bc897e2b
Manuela183 <mamarulandaa@unal.edu.co>
2022-05-02 12:57:53 -0500

    Cambios para alerta de input

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //  let validarRadio = document.getElementById('flexRadioDefault1')
 //validarRadio.onclick = function(){desplegar()};
 
@@ -10,9 +9,6 @@ function desplegar(){
 let verificarContrasena = document.getElementById('verificarContrasena')
 
 function validarContrasena(){
-=======
-function verificarContrasena(){
->>>>>>> 9c885524c57a9119e723e1d8b9f89bfb636d55ee
     let contrasena = document.getElementById('contrasena')
     let verificarContrasena = document.getElementById('verificarContrasena')
     if (contrasena == verificarContrasena){
@@ -20,4 +16,10 @@ function verificarContrasena(){
     }else{
         console.log("no")
     }
+}
+
+function pruebas(){
+    let mensaje = document.getElementById("nombre").value;
+    mensaje = mensaje.toUpperCase();
+    alert("Entrada: " + mensaje)
 }
