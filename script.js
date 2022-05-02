9cf8556b842873cfd30a1938eece1655d691aec4
Juan Giraldo <juandiegogbo@gmail.com>
2022-05-02 11:21:20 -0500

    Prueba

---
+++ script.js
@@ -0,0 +1,8 @@
+let verificarContrasena = document.getElementById('verificarContrasena')
+
+function validarContrasena(){
+    let contrasena = document.getElementById('contrasena')
+    
+
+}
+    
